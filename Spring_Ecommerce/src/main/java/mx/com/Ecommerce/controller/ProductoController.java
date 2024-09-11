52de0e1c4803442b6e3aece19314850911980ed9
angelguzman97 <109249659+angelguzman97@users.noreply.github.com>
2024-09-11 03:43:27 -0600

    Vista editar, botón y controlador actualizar

--- Spring_Ecommerce/src/main/java/mx/com/Ecommerce/controller/ProductoController.java
+++ Spring_Ecommerce/src/main/java/mx/com/Ecommerce/controller/ProductoController.java
@@ -1,11 +1,15 @@
 package mx.com.Ecommerce.controller;
 
+import java.util.Optional;
+
 import org.slf4j.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import mx.com.Ecommerce.model.Producto;
@@ -38,6 +42,27 @@ public class ProductoController {
 	@PostMapping("/save")
 	public String save(Producto producto) {
 		LOGGER.info("Este es el objeto del producto {}",producto);
+		Usuario u= new Usuario(1, "", "", "", "", "", "", "");
+		producto.setUsuario(u);
+		productoService.save(producto);
+		return "redirect:/productos";
+	}
+	
+	@GetMapping("/edit/{id}")
+	public String edit(@PathVariable Long id, Model model) {
+		Producto producto=new Producto();
+		Optional<Producto> optionalProducto=productoService.get(id);
+		producto = optionalProducto.get();
+		
+		LOGGER.info("Producto buscado: {}",producto);
+		model.addAttribute("producto",producto);
+		
+		return "productos/edit";
+	}
+	
+	@PostMapping("/update")
+	public String update(Producto producto) {
+		LOGGER.info("Producto Actualizado {}",producto);
 		Usuario u= new Usuario(1, "", "", "", "", "", "", "");
 		producto.setUsuario(u);
 		productoService.save(producto);
