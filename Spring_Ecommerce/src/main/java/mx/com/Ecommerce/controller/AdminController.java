e6c64c512928922878f1328087a824b5c84224cc
angelguzman97 <109249659+angelguzman97@users.noreply.github.com>
2024-09-13 03:03:20 -0600

    Mostrar las imágenes en el home y en la plantilla de editar

--- Spring_Ecommerce/src/main/java/mx/com/Ecommerce/controller/AdminController.java
+++ Spring_Ecommerce/src/main/java/mx/com/Ecommerce/controller/AdminController.java
@@ -1,15 +1,29 @@
 package mx.com.Ecommerce.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import mx.com.Ecommerce.model.Producto;
+import mx.com.Ecommerce.service.ProductoService;
 
 @Controller
 @RequestMapping("/administrador")
 public class AdminController {
 	
+	@Autowired
+	private ProductoService productoService;
+	
 	@GetMapping("")
-	public String home() {
+	public String home(Model model) {
+		
+		List<Producto> productos = productoService.findAll();
+		model.addAttribute("productos",productos);
+		
 		return "administrador/home";
 	}
 
--- Spring_Ecommerce/src/main/java/mx/com/Ecommerce/controller/ProductoController.java
+++ Spring_Ecommerce/src/main/java/mx/com/Ecommerce/controller/ProductoController.java
@@ -24,76 +24,101 @@ import mx.com.Ecommerce.service.UploadFileService;
 @RequestMapping("/productos")
 public class ProductoController {
 
-	//Hacer pruebas y mostrar por consola
+	// Hacer pruebas y mostrar por consola
 	private final Logger LOGGER = LoggerFactory.getLogger(ProductoController.class);
-	
+
 	@Autowired
 	private ProductoService productoService;
-	
+
 	@Autowired
 	private UploadFileService upload;
-	
+
 	@GetMapping("")
-	public String show(Model model) { //El objeto Model lleva información desde el Back hasta la vista
-		//El primer parametro es el nombre con quien voy a recibir la información, en este caso es productos.
-		//El segundo parametro es el objeto/variable que mostraremos en la vista
+	public String show(Model model) { // El objeto Model lleva información desde el Back hasta la vista
+		// El primer parametro es el nombre con quien voy a recibir la información, en
+		// este caso es productos.
+		// El segundo parametro es el objeto/variable que mostraremos en la vista
 		model.addAttribute("productos", productoService.findAll());
 		return "productos/show";
 	}
-	
+
 	@GetMapping("/create")
 	public String create() {
 		return "productos/create";
 	}
-	
+
 	@PostMapping("/save")
-	public String save(Producto producto,@RequestParam("img") MultipartFile file) throws IOException {
-		LOGGER.info("Este es el objeto del producto {}",producto);
-		Usuario u= new Usuario(1, "", "", "", "", "", "", "");
+	public String save(Producto producto, @RequestParam("img") MultipartFile file) throws IOException {
+		LOGGER.info("Este es el objeto del producto {}", producto);
+		Usuario u = new Usuario(1, "", "", "", "", "", "", "");
 		producto.setUsuario(u);
-		
-		//Se coloca la lógica de la imagen también.
-		if(producto.getId() == null) {//Cuando se crea un producto
-			String nombreImagen = upload.saveImage(file);//Guardar el nombre de la imagen
+		// Se coloca la lógica de la imagen
+		if (producto.getId() == null) {// Cuando se crea un producto
+			String nombreImagen = upload.saveImage(file);// Guardar el nombre de la imagen
 			producto.setImagen(nombreImagen);
 		}else {
-			if(file.isEmpty()) { //editamos el producto, pero no se cambia la imagen
-				Producto p = new Producto();
-				p = productoService.get(producto.getId()).get();
-				producto.setImagen(p.getImagen());
-			}else {//Editamos el producto completo (con todo e imagen)
-				String nombreImagen = upload.saveImage(file);//Guardar el nombre de la imagen
-				producto.setImagen(nombreImagen);
-			}
-		}
-		
+			
+		}		
+
 		productoService.save(producto);
 		return "redirect:/productos";
 	}
-	
+
 	@GetMapping("/edit/{id}")
 	public String edit(@PathVariable Long id, Model model) {
-		Producto producto=new Producto();
-		Optional<Producto> optionalProducto=productoService.get(id);
+		Producto producto = new Producto();
+		Optional<Producto> optionalProducto = productoService.get(id);
 		producto = optionalProducto.get();
-		
-		LOGGER.info("Producto buscado: {}",producto);
-		model.addAttribute("producto",producto);
-		
+
+		LOGGER.info("Producto buscado: {}", producto);
+		model.addAttribute("producto", producto);
+
 		return "productos/edit";
 	}
-	
+
 	@PostMapping("/update")
-	public String update(Producto producto) {
-		LOGGER.info("Producto Actualizado {}",producto);
-		Usuario u= new Usuario(1, "", "", "", "", "", "", "");
-		producto.setUsuario(u);
+	public String update(Producto producto, @RequestParam("img") MultipartFile file) throws IOException {
+				
+		if (file.isEmpty() || file.equals(producto.getNombre())) { // editamos el producto, pero no se cambia la imagen	
+			Usuario u = new Usuario(1, "", "", "", "", "", "", "");
+			producto.setUsuario(u);
+			
+			Producto p = new Producto();
+			p = productoService.get(producto.getId()).get();
+			producto.setImagen(p.getImagen());
+		} else {// Editamos la imagen del producto
+			
+			Producto p = new Producto();
+			p = productoService.get(producto.getId()).get();
+			
+			// Eliminar cuando no sea la imagen por defecto. Editar y se reemplace,
+			// eliminando la anterior sin afectar del de por defecto
+			if (!p.getImagen().equals("default.jpg")) {
+				upload.deleteImage(p.getImagen());
+			}
+			Usuario u = new Usuario(1, "", "", "", "", "", "", "");
+			producto.setUsuario(u);
+			String nombreImagen = upload.saveImage(file);// Guardar el nombre de la imagen
+			producto.setImagen(nombreImagen);
+			
+		}
+
 		productoService.save(producto);
 		return "redirect:/productos";
 	}
-	
+
 	@GetMapping("/delete/{id}")
 	public String delete(@PathVariable Long id) {
+
+		// Eliminar imagen
+		Producto p = new Producto();
+		p = productoService.get(id).get();
+		
+		// Eliminar cuando no sea la imagen por defecto
+		if (!p.getImagen().equals("default.jpg")) {
+			upload.deleteImage(p.getImagen());
+		}
+
 		productoService.delete(id);
 		return "redirect:/productos";
 	}
