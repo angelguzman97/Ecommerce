ba212fcd75e6ad7f267d5c45b43aaa3c8e2cac26
angelguzman97 <109249659+angelguzman97@users.noreply.github.com>
2024-09-17 19:15:32 -0600

    Funcionalidad para eliminar un producto del carrito de compras

--- Spring_Ecommerce/src/main/java/mx/com/Ecommerce/controller/HomeController.java
+++ Spring_Ecommerce/src/main/java/mx/com/Ecommerce/controller/HomeController.java
@@ -20,76 +20,107 @@ import mx.com.Ecommerce.model.Producto;
 import mx.com.Ecommerce.service.ProductoService;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
 @Controller
 @RequestMapping("/")
 public class HomeController {
-	
+
 	private final Logger log = LoggerFactory.getLogger(HomeController.class);
-	
+
 	@Autowired
 	private ProductoService productoService;
-	
-	//Para almacenar los detalles de la orden
+
+	// Para almacenar los detalles de la orden en una lista
 	List<DetalleOrden> detalles = new ArrayList<DetalleOrden>();
-	
-	//Datos de la orden
+
+	// Datos de la orden
 	Orden orden = new Orden();
-	
+
 	@GetMapping("")
 	public String home(Model model) {
-		model.addAttribute("productos",productoService.findAll());
-		
+		model.addAttribute("productos", productoService.findAll());
+
 		return "usuario/home";
 	}
-	
-	//Método para ver los productos individualmente
+
+	// Método para ver los productos individualmente
 	@GetMapping("productohome/{id}")
 	public String productoHome(@PathVariable Long id, Model model) {
 		log.info("id producto enviado como parámetro {}", id);
-		
+
 		Producto producto = new Producto();
 		Optional<Producto> productoOptional = productoService.get(id);
 		producto = productoOptional.get();
-		
+
 		model.addAttribute("producto", producto);
-		
+
 		return "usuario/productohome";
 	}
-	
+
 	@PostMapping("/cart")
 	public String addCart(@RequestParam Long id, @RequestParam Integer cantidad, Model model) {
 		DetalleOrden detalleOrden = new DetalleOrden();
 		Producto producto = new Producto();
 		double sumaTotal = 0;
-		
+
 		Optional<Producto> optionalProducto = productoService.get(id);
 		log.info("Producto añadido: {}", optionalProducto.get());
 		log.info("Cantidad: {}", cantidad);
-		
+
 		producto = optionalProducto.get();
-		
+
 		detalleOrden.setCantidad(cantidad);
 		detalleOrden.setPrecio(producto.getPrecio());
 		detalleOrden.setNombre(producto.getNombre());
-		detalleOrden.setTotal(producto.getPrecio()*cantidad);
+		detalleOrden.setTotal(producto.getPrecio() * cantidad);
 		detalleOrden.setProducto(producto);
-		
-		//Añadir cada detalleOrden a detalles. Para posteriormente hacer la sumatoria
+
+		// Añadir cada detalleOrden a detalles. Para posteriormente hacer la sumatoria
 		detalles.add(detalleOrden);
-		
-		//Función lambda. Hacer el conteo total que contenga una lista
-		sumaTotal = detalles.stream().mapToDouble(dt-> dt.getTotal()).sum();
-		
+
+		// Función lambda. Hacer el conteo total que contenga una lista
+		sumaTotal = detalles.stream().mapToDouble(dt -> dt.getTotal()).sum();
+
 		orden.setTotal(sumaTotal);
-		
-		//Mostrar eso en la vista.
+
+		// Mostrar eso en la vista.
 		model.addAttribute("cart", detalles);
-		//Pasar la orden como tal
-		model.addAttribute("orden",orden);
-		
+		// Pasar la orden como tal
+		model.addAttribute("orden", orden);
+
 		return "usuario/carrito";
 	}
-	
+
+	// Quitar un producto del carrito
+	@GetMapping("/delete/cart/{id}")
+	public String deleteProductoCart(@PathVariable Long id, Model model) {
+
+		// Lista nueva de productos
+		List<DetalleOrden> ordenesNueva = new ArrayList<DetalleOrden>();
+
+		// Recorrer la lista de detalles con un for
+		for (DetalleOrden detalleOrden : detalles) {
+			// Hacer una validación. Si encuentra un Id diferente lo va a añadir
+			if (detalleOrden.getProducto().getId() != id) {
+				ordenesNueva.add(detalleOrden);
+			}
+		}
+
+		// Poner la nueva lista con los productos restantes
+		detalles = ordenesNueva;
+
+		double sumaTotal = 0;
+
+		// Función lambda. Hacer el conteo total que contenga una lista
+		sumaTotal = detalles.stream().mapToDouble(dt -> dt.getTotal()).sum();
+
+		orden.setTotal(sumaTotal);
+
+		// Mostrar eso en la vista.
+		model.addAttribute("cart", detalles);
+		// Pasar la orden como tal
+		model.addAttribute("orden", orden);
+
+		return "usuario/carrito";
+	}
 
 }
