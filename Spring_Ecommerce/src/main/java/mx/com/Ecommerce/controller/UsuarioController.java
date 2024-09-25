df77407dfee6b180d40768c47e3ecd6c5d73dd6e
angelguzman97 <109249659+angelguzman97@users.noreply.github.com>
2024-09-25 01:08:21 -0600

    Mostrar la vista compras para usuario

--- Spring_Ecommerce/src/main/java/mx/com/Ecommerce/controller/UsuarioController.java
+++ Spring_Ecommerce/src/main/java/mx/com/Ecommerce/controller/UsuarioController.java
@@ -6,12 +6,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import jakarta.servlet.http.HttpSession;
 import mx.com.Ecommerce.model.Usuario;
+import mx.com.Ecommerce.service.IDetalleOrdenService;
 import mx.com.Ecommerce.service.IUsuarioService;
 
 @Controller
@@ -22,6 +24,9 @@ public class UsuarioController {
 
 	@Autowired
 	private IUsuarioService usuarioService;
+	
+	@Autowired
+	private IDetalleOrdenService detalleService;
 
 	@GetMapping("/registro")
 	public String create() {
@@ -73,5 +78,14 @@ public class UsuarioController {
 		}
 
 		return "redirect:/";
+	}
+	
+	@GetMapping("/compras")
+	public String obtenerCompras(Model model, HttpSession session) {
+		
+		model.addAttribute("sesion", session.getAttribute("idusuario"));
+		
+		
+		return "usuario/compras";
 	}
 }
