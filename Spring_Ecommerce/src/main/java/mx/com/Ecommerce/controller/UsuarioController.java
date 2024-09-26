b770dbac751889873835684588190eca91bf7794
angelguzman97 <109249659+angelguzman97@users.noreply.github.com>
2024-09-25 18:09:10 -0600

    Cerrar la sesión del usuario

--- Spring_Ecommerce/src/main/java/mx/com/Ecommerce/controller/UsuarioController.java
+++ Spring_Ecommerce/src/main/java/mx/com/Ecommerce/controller/UsuarioController.java
@@ -112,4 +112,12 @@ public class UsuarioController {
 		
 		return "usuario/detallecompra";
 	}
+	
+	@GetMapping("/cerrar")
+	public String cerrarSession(HttpSession session) {
+		
+		session.removeAttribute("idusuario");
+		
+		return "redirect:/";
+	}
 }
